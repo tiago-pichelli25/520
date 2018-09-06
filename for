b945df8c7f41fa539d7e395548335214ead5a692
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-09-06 17:01:14 -0300

    aula1

---
+++ for
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+letras = []
+
+for x in range(97, 97+26, 1):
+    letras.append(chr(x))
+
+print(letras)
+
+
---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+nome =input('Digite seu nome')
+idade =input('Idade') #Digitar algo input 'texto' (variavel nome para mostrar na tela)
+
+print('Seja bem vindo', nome,idade)
+nome = "Tiago"
+sobrenome = "Pichelli"
+print(nome,sobrenome, sep=' ' , end='\n\n')
+
+
+
+      
